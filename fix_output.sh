96a7b9bf1ce9ecf09384dfebda36f3229f68b33b
Pau Fernández <pau.fernandez@upc.edu>
2014-10-05 12:42:36 +0200

    Poso comentaris al script fix_output.sh

--- fix_output.sh
+++ fix_output.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Canviar el theme a world per aprofitar els estils en blau
 find output -name "*.html" | while read f; do
    sed -i 's/catalonia/world/' $f
 done
 
+# Utilitzar estils reservats per 'en-GB' només?
 sed -i 's/en-GB/ca-ES/g' output/css/master.min.css
 
+# Retoc de CSS
 echo ".lesson-steps ul { margin-left: 1.4em; }" >> output/css/master.min.css
