d84216a74d8df9c73b005668b3d5203d22e52c63
Pau Fernández <pau.fernandez@upc.edu>
2014-10-09 10:02:32 +0200

    Bug fixes + fix CSS for all languages

--- fix_output.sh
+++ fix_output.sh
@@ -2,11 +2,11 @@
 
 # Canviar el theme a world per aprofitar els estils en blau
 find output -name "*.html" | while read f; do
-   sed -i 's/catalonia/world/' $f
+   sed -i 's/catalonia/world/' "$f"
 done
 
 # Utilitzar estils reservats per 'en-GB' només?
-sed -i 's/en-GB/ca-ES/g' output/css/master.min.css
+sed -i 's/lang="en-GB"/lang/g' output/css/master.min.css
 
 # Retoc de CSS
 echo ".lesson-steps ul { margin-left: 1.4em; }" >> output/css/master.min.css
